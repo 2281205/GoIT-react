50e39c4569efe7542067029451cae70af6bbf9b8
MikeMerzliakov <2281205@gmail.com>
2023-03-31 16:47:04 +0300

    CV

---
+++ goit-CV/js/info.js
@@ -0,0 +1,105 @@
+const info = {
+    img: './images/me.png',
+    contacts: ['Contacts',[`cellPhone`,'+380937099146'],'2281205@gmail.com',[`GIT`,'https://github.com/2281205'],[`linkedin`,'https://www.linkedin.com/in/mike-merzliakov/']],
+    techSkills: ['Tech skills','HTML5','CSS3','GIT','WebPack','JavaScript','React.js','Node.js','REST API','Scrum','Agile','GTD','Note++'],
+    softSkills: ['Soft skills','Accurate','Hardworking','Ability to work efficiently both individually and in a team','Quick learning skills','Willingness to learn','Punctual'],
+    languageSkills: ['Language skills','Ukrainian — native','Russian — native','English — intermediate']
+};
+
+const cv = {
+    title: 'CV_Mike_Merzliakov',
+    name: 'Mike',
+    surname: 'Merzliakov',
+    targetCV: 'Junior Frontend Developer',
+    summaryCV: 'I am a Junior Front-End developer with knowledge of HTML, CSS, JavaScript and React. I am looking for a job. English — Intermediate; I am communicative, hard-working and purposeful, responsible and always up for a challenge. I am an easy and fast learner, always open to new knowledge and skills. Development, practice and work for the result are important for me. I am skillful in analyzing problems and thinking of solve technical problems.',
+    additionalinfoCV: '+',
+    projExperience: [
+        'table',
+        `Project Experience`,
+        {
+            id:1,
+            nameProj: 'Icecream project  ',
+            skillProj: 'HTML, CSS, JavaScript',
+            link: 'https://github.com/2281205',
+            linkGit: 'https://github.com/2281205'
+        },
+        {
+            id:2,
+            nameProj: 'Filmoteka',
+            skillProj: 'HTML, SCSS, JavaScript',
+            link:'https://github.com/2281205',
+            linkGit:'https://github.com/2281205'
+        },
+        {    
+            id:3,
+            nameProj: 'Image finder',
+            skillProj:  'HTML, CSS, JavaScript, React.js',
+            link:'https://github.com/2281205',
+            linkGit:'https://github.com/2281205'
+        },
+        {
+            id:4,
+            nameProj: 'WebSudio',
+            skillProj: 'HTML, SCSS, JavaScript',
+            link:'https://github.com/2281205',
+            linkGit:'https://github.com/2281205'
+        },
+        {
+            id:5,
+            nameProj: 'Other',
+            skillProj: 'HTML, CSS, JavaScript, React.js',
+            link:'https://github.com/2281205',
+            linkGit:'https://github.com/2281205'
+        }
+    ],
+    workExperience: [
+        'inline',
+        `Work Experience`,
+        {
+        id:1,
+        name: 'Windows engenieer',
+        Corp: 'Self-employment',
+        dataOn: 'March 2022',
+        dataOff: 'now',
+        place: 'Kyiv',
+        about: '...1'
+        },
+        {
+        id:2,
+        name: 'Senior system administrator',
+        Corp: '"Nasha-Karta" group',
+        dataOn: 'May 2012',
+        dataOff: 'October 2022',
+        place: 'Kyiv',
+        about: '...2'
+        }
+    ],
+    education: [
+        'inline',
+        `Education`,
+        {
+        id:1,
+        name: 'system engineer',
+        Corp: 'Kyiv University KPI',
+        dataOn: 'September 2005',
+        dataOff: 'June 2011',
+        degree: 'Master',
+        about: '...1'
+        }
+    ],
+    addEducation: [
+        'inline',
+        `Additional Education`,
+        {
+        id:2,
+        name: 'Junior Javascript Developer',
+        Corp: 'Hillel -IT school, Kyiv',
+            dataOn: 'April 2021',
+            dataOff: 'April 2022',
+            degree: 'Master',
+            about: '...1'
+            }
+    ]
+};
+
+export {info, cv } 
