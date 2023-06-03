b850887b4d4dbd3387fbefaa6078a73da0171716
Mike Merzliakov <2281205@gmail.com>
2023-06-03 04:29:16 +0300

    add usersDB file

---
+++ goit-react-goose-track/src/components/usersDB.js
@@ -0,0 +1,33 @@
+export const users = [
+    { 
+     name: "Olena Doe",
+     star: 4,
+     text: "GooseTrack is impressive, the calendar view and filter options make it easy to stay organized and focused. Highly recommended.",
+     avatar: require('../img/avatars/Olena.jpg'),
+    },
+    { 
+     name: "Alexander Hubbard",
+     star: 3,
+     text: "GooseTrack is incredibly helpful, the sidebar with account management, calendar, and filter options make navigation seamless. Great for staying organized.",
+     avatar: require('../img/avatars/Alexander.jpg'),
+    },
+    { 
+     name: "Lisa Olloc",
+     star: 3,
+     text: "Good!",
+     avatar: require('../img/avatars/Lisa.jpg'),
+    },
+    { 
+     name: "tiim",
+     star: 5,
+     text: "Excellent!!",
+     avatar: require('../img/avatars/Tim.jpg'),
+    },
+    { 
+     name: "Graph",
+     star: 1,
+     text: "BAD((",
+     avatar: require('../img/avatars/graph.png'),
+    },
+ ];
+ 
--- goit-react-goose-track/src/pages/MainPage.jsx
+++ goit-react-goose-track/src/pages/MainPage.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import Slider from "react-slick";
 
 import { Comment } from "../components/Comment";
+import { users } from "../components/usersDB"
 import { MainPage } from "./MainPage.styled";
 import sprite from '../img/icons/sprite.svg';
-
-import Olena from '../img/avatars/Olena.jpg';
-import Alexander from '../img/avatars/Alexander.jpg'
-import Lisa from '../img/avatars/Lisa.jpg'
-import Tim from '../img/avatars/Tim.jpg'
-import graph from '../img/avatars/graph.png'
 
 const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
   <div {  ...props} 
@@ -57,18 +52,14 @@ export function StartPageMain(){
         prevArrow:<SlickArrowLeft></SlickArrowLeft>
       }
     
-  return(
-        <MainPage>
-            <section className="content-review">
-                <h2 className="content-h2">Reviews</h2>
-                <Slider  className="slider"  {...settings} >
-                <Comment className='tr' src={Olena} star={4} name='Olena Doe'>GooseTrack is impressive, the calendar view and
-                    filter options make it easy to stay organized and focused. Highly recommended.</Comment>
-                <Comment className='tr' src={Alexander} star={4} name='Alexander Hubbard'>GooseTrack is incredibly helpful, the sidebar with account management, 
-                calendar, and filter options make navigation seamless. Great for staying organized.</Comment>   
-                <Comment className='tr' src={Lisa} star={3} name='Lisa Olloc'>Good!</Comment>  
-                <Comment className='tr' src={Tim} star={5} name='Tim'>Excellent!</Comment>      
-                <Comment className='tr' src={graph} star={2} name='graph'>bad!</Comment>      
-                </Slider>
-            </section>
-        </MainPage>);}
+return(
+    <MainPage>
+        <section className="content-review">
+            <h2 className="content-h2">Reviews</h2>
+            <Slider  className="slider"  {...settings} >
+                {users.map( ({ name, star, text, avatar }, index) =>
+                    <Comment key={index} className='tr' src={avatar} star={star} name={name}>{text}</Comment>)
+                }
+            </Slider>
+        </section>
+    </MainPage>);}
